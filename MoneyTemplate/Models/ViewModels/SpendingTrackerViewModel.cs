7be1c46b614ad6f721f6dbb67792afb9d891e5d3
Rex <im@rex.tw>
2018-07-10 00:22:00 +0800

    SpendingTrackerViewModel.cs： DATE資料型態由String改成DateTime 增加DataFormatString

--- MoneyTemplate/Models/ViewModels/SpendingTrackerViewModel.cs
+++ MoneyTemplate/Models/ViewModels/SpendingTrackerViewModel.cs
@@ -1,23 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MoneyTemplate.Models.ViewModels
 {
     public class SpendingTrackerViewModel
     {
         [DisplayName("類別")]
-        public String TYPE { get; set; }
+        public TypeEnum TYPE { get; set; }
 
         [DisplayName("日期")]
-        public String DATE { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DATE { get; set; }
 
         [DisplayName("金額")]
+        [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = true)]
         public int AMOUMT { get; set; }
 
         [DisplayName("備註")]
         public String REMARK { get; set; }
+    }
+
+    public enum TypeEnum
+    {
+        收入 = 'D',
+        支出 = 'C'
     }
 }
