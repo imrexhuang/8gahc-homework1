f7d0e946ca34bc184a43c6a588900d5f8a76c707
Rex <im@rex.tw>
2018-07-28 08:58:03 +0800

    UnitOfWork和Repository

---
+++ MoneyTemplate/Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using MoneyTemplate.Models;
+
+namespace MoneyTemplate.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public DbContext Context { get; set; }
+
+        public UnitOfWork()
+        {
+            Context = new ModelContext();
+        }
+
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
+
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+    }
+}
