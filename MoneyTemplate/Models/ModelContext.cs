8d156218257ada2c12030782c462145a768acd31
Rex <im@rex.tw>
2018-07-28 08:59:08 +0800

    第二次回家作業 完成CRUD的R和U

---
+++ MoneyTemplate/Models/ModelContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace MoneyTemplate.Models
+{
+    public class ModelContext : DbContext
+    {
+        public ModelContext() : base("name=SkillTreeHomeworkEntities")
+        {
+        }
+        public virtual DbSet<AccountBook> AccountBook { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+        }
+
+        public System.Data.Entity.DbSet<MoneyTemplate.Models.ViewModels.SpendingTrackerViewModel> SpendingTrackerViewModels { get; set; }
+    }
+}
