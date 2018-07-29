28ec25a3e028325172055cda72f3013359d38010
Rex <im@rex.tw>
2018-07-29 12:40:36 +0800

    管理頁面雛型(Adrea)

---
+++ MoneyTemplate/Areas/MBAdmin/MBAdminAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace MoneyTemplate.Areas.MBAdmin
+{
+    public class MBAdminAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "MBAdmin";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "MBAdmin_default",
+                "MBAdmin/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
