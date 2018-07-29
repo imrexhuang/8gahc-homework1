8f5939d0e2fe7283082d4d8b2fc417a4f2397c5a
Rex <im@rex.tw>
2018-07-29 12:11:18 +0800

    override , add log

--- MoneyTemplate/Models/ModelContext.cs
+++ MoneyTemplate/Models/ModelContext.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Linq;
 
 namespace MoneyTemplate.Models
 {
@@ -14,5 +16,33 @@ namespace MoneyTemplate.Models
         }
 
         public System.Data.Entity.DbSet<MoneyTemplate.Models.ViewModels.SpendingTrackerViewModel> SpendingTrackerViewModels { get; set; }
+
+
+        // https://dotblogs.com.tw/wasichris/2015/01/24/148255
+        public override int SaveChanges()
+        {
+            try
+            {
+                return base.SaveChanges();
+            }
+            catch (DbEntityValidationException ex)
+            {
+                // Retrieve the error messages as a list of strings.
+                var errorMessages = ex.EntityValidationErrors
+                        .SelectMany(x => x.ValidationErrors)
+                        .Select(x => x.ErrorMessage);
+
+                // Join the list to a single string.
+                var fullErrorMessage = string.Join("; ", errorMessages);
+
+                // Combine the original exception message with the new one.
+                var exceptionMessage =
+                          string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                // Throw a new DbEntityValidationException with the improved exception message.
+                throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
+            }
+        }   
+
     }
 }
