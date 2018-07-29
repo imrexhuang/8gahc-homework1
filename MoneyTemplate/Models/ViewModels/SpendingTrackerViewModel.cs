4f41a448223da72c46c6775978008068d6f61637
Rex <im@rex.tw>
2018-07-29 13:58:18 +0800

    「日期」不得大於今天

--- MoneyTemplate/Models/ViewModels/SpendingTrackerViewModel.cs
+++ MoneyTemplate/Models/ViewModels/SpendingTrackerViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoneyTemplate.Attribute;
+using System;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
@@ -7,18 +8,24 @@ namespace MoneyTemplate.Models.ViewModels
     public class SpendingTrackerViewModel
     {
         [Key]
+        [Required]
         public Guid ID { get; set; }
 
         [DisplayName("類別")]
+        [Range(0, 1)]
+        [Required]
         public int TYPE { get; set; }
 
         [DisplayName("日期")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Required]
+        [CheckDateEarilerThanToday]
         public DateTime DATE { get; set; }
 
         [DisplayName("金額")]
         [DisplayFormat(DataFormatString = "{0:N0}", ApplyFormatInEditMode = false)]
+        [Range(1, 999999999999)] //最小1元,最大不可到1兆
+        [Required]
         public int AMOUMT { get; set; }
 
         [DisplayName("備註")]
